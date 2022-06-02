03e99d18805e9589311eb6855194c6d1ce7c39e9
avendo <leon.kranjcevic@outlook.com>
2022-06-02 02:23:43 +0200

    Added RGB led strip room support, temperature and humidity live data reading

--- main/CLed.h
+++ main/CLed.h
@@ -3,25 +3,26 @@
 #ifndef _CLed_h
 #define _CLed_h
 
-enum LedStatus{ OFF, ON, BLINK, FAST_BLINK, SLOW_BLINK};
+enum LedStatus
+{
+    OFF,
+    ROOM1,
+    ROOM2,
+    ALL
+};
 
+class CLed
+{
 
-#define LED_BLINK_FAST   250000L
-#define LED_BLINK       1000000L
-#define LED_BLINK_SLOW  2000000L
+public:
+    CLed(int port, int channel);
+    void tick();
+    void setLedState(LedStatus x);
 
-class CLed{
-    
-    public:
-        CLed(int port);
-        void tick();
-        void toogleLedState(int toogle);
-    
-    private:
-        LedStatus m_state = OFF;
-        gpio_num_t m_pinNumber;
-        led_strip_t *pStrip_a;
-        uint8_t s_led_state = 0;
-        const char *LogName = "CLed";
+private:
+    LedStatus m_state = OFF;
+    gpio_num_t m_pinNumber;
+    led_strip_t *pStrip_a;
+    const char *LogName = "CLed";
 };
 #endif
